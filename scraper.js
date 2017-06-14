0c8d12fedfd1f0c70549f8202daada0d175da3a0
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-14 23:38:04 +0300

    Create scraper.js

--- scraper.js
+++ scraper.js
@@ -113,22 +113,22 @@ nest.forEach(function(item) {
 	
 	
 	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data1 (item TEXT,countNo INT,countOpen INT)");
-		var statement = db.prepare("INSERT INTO data1 VALUES (?,?,?)");
+		db.run("CREATE TABLE IF NOT EXISTS data1 (item TEXT,countNo INT,countOpen INT,totalNo INT,totalOpen INT)");
+		var statement = db.prepare("INSERT INTO data1 VALUES (?,?,?,?,?)");
 
 
 if(item.values.length==2){
-	statement.run(item.key,item.values[0].value.count,item.values[1].value.count); 
+	statement.run(item.key,item.values[0].value.count,item.values[1].value.count,item.values[0].value.total,item.values[1].value.total); 
 	//console.log(2)
 }
 else {	
 	if(item.values[0].key==""){
 		//console.log("nathing")
-		statement.run(item.key,item.values[0].value.count,0);
+		statement.run(item.key,item.values[0].value.count,0,item.values[0].value.total,0);
 	}
 	if(item.values[0].key=="open"){
 		//console.log("open")
-		statement.run(item.key,0,item.values[0].value.count);
+		statement.run(item.key,0,item.values[0].value.count,0,item.values[0].value.total);
 	}
 }
 		
