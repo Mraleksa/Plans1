c4234dc6c8f8ff536e22d2eee92abfddcbf8c067
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-15 20:26:32 +0300

    Create scraper.js

--- scraper.js
+++ scraper.js
@@ -143,7 +143,7 @@ statement.finalize();
 
 					
 					
-exporter.json('SELECT item,countNo,countOpen,totalNo,totalOpen FROM data_nest1', function (err, data_nest_json) {
+db.run('SELECT item,countNo,countOpen,totalNo,totalOpen FROM data_nest1', function (err, data_nest_json) {
 	console.log(data_nest_json);
 })
 				
